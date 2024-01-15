8796514f8f0929d2de0602020048a6279c2e6e93
Kopal Mehta <kopal.mehta22@gmail.com>
2024-01-15 16:18:01 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <div className="card">
+    <input type="search" />
+    <button onClick={bu}>Submit</button>
+   </div>
+    </div>
+  );
+}
+
+export default App;
